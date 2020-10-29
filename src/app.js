7d08955b74944cc5c8936a34f1d6422ec01ea357
Gabriel Borges <gbdesigns13@gmail.com>
2020-10-29 12:19:20 -0300

    Envio do desafio final

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const {title, url, techs} = request.body;
+
+    const repositorie = { id: uuid(), title, url, techs, likes: 0};
+
+    repositories.push(repositorie);
+
+    return response.json(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex < 0) {
+    return response.status(400).json({error: 'Repositorie not found'})
+  }
+
+  const repositorie = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  }
+
+  repositories[repositorieIndex] = repositorie;
+
+  return response.json(repositorie);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex === -1) {
+    return response.status(400).json({error: 'Repositorie not found'})
+  }
+
+  repositories.splice(repositorieIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id)
+
+  if (repositorieIndex === -1) {
+    return response.status(400).json({error: 'Repositorie not found'})
+  }
+
+  repositories[repositorieIndex].likes++
+  return response.json(repositories[repositorieIndex])
 });
 
 module.exports = app;
